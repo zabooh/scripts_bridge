bca56f7c57f688ee0b28d93662ab6d8cc3afd75e
sponkytoo <mafrtin.ruppert@microchip.com>
2022-02-09 23:46:19 +0100

    import

---
+++ git_clone_bridge.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+
+set H3_APP_GITHUB="https://github.com/Microchip-MPLAB-Harmony"
+set APP_BRIDGE_GITHUB="https://github.com/zabooh"
+
+set H3_INSTALL_APP="..\frameworks\app"
+set H3_INSTALL_BOOTLOADER="..\frameworks\bootloader"
+
+set H3_INSTALL_APP_APPS="..\apps\apps"
+set H3_INSTALL_BOOTLOADER_APPS="..\apps\bootloader_apps"
+
+
+git clone -b "v3.10.0"              %H3_APP_GITHUB%/core.git %H3_INSTALL_APP%/core
+git clone -b "v3.10.0"              %H3_APP_GITHUB%/csp.git  %H3_INSTALL_APP%/csp
+git clone -b "v3.6.0"               %H3_APP_GITHUB%/wireless_wifi.git %H3_INSTALL_APP%/wireless_wifi
+git clone -b "v3.8.1"               %H3_APP_GITHUB%/usb.git %H3_INSTALL_APP%/usb
+git clone -b "v3.10.0"              %H3_APP_GITHUB%/dev_packs.git %H3_INSTALL_APP%/dev_packs
+git clone -b "v3.6.0"               %H3_APP_GITHUB%/wireless_system_pic32mzw1_wfi32e01.git %H3_INSTALL_APP%/wireless_system_pic32mzw1_wfi32e01
+git clone -b "v4.7.0"               %H3_APP_GITHUB%/wolfssl.git %H3_INSTALL_APP%/wolfssl
+git clone -b "v3.7.4"               %H3_APP_GITHUB%/net.git %H3_INSTALL_APP%/net
+git clone -b "v3.7.4"               %H3_APP_GITHUB%/crypto.git %H3_INSTALL_APP%/crypto
+git clone -b "v10.3.1"              %H3_APP_GITHUB%/CMSIS-FreeRTOS.git %H3_INSTALL_APP%/CMSIS-FreeRTOS
+git clone -b "v3.8.2"               %H3_APP_GITHUB%/mhc.git %H3_INSTALL_APP%/mhc
+
+
+git clone -b "v3.9.1"               %H3_APP_GITHUB%/csp.git  %H3_INSTALL_BOOTLOADER%/csp
+git clone -b "v3.9.1"               %H3_APP_GITHUB%/core.git  %H3_INSTALL_BOOTLOADER%/core
+git clone -b "v3.7.2"               %H3_APP_GITHUB%/usb.git  %H3_INSTALL_BOOTLOADER%/usb
+git clone -b "v3.9.0"               %H3_APP_GITHUB%/dev_packs.git  %H3_INSTALL_BOOTLOADER%/dev_packs
+git clone -b "v3.5.0"               %H3_APP_GITHUB%/bootloader.git  %H3_INSTALL_BOOTLOADER%/bootloader
+git clone -b "v3.9.0"               %H3_APP_GITHUB%/bsp.git  %H3_INSTALL_BOOTLOADER%/bsp
+git clone -b "v3.8.2"               %H3_APP_GITHUB%/mhc.git  %H3_INSTALL_BOOTLOADER%/mhc
+
+
+git clone -b "v3.1.0"               %H3_APP_GITHUB%/bootloader_apps_usb.git  %H3_INSTALL_BOOTLOADER_APPS%/bootloader_apps_usb
+git clone -b "v1.0.0"               %APP_BRIDGE_GITHUB%/wfi32_bridge.git   %H3_INSTALL_APP_APPS%/wfi32_bridge
---
+++ git_describe_bridge.bat
@@ -0,0 +1,63 @@
+@ECHO OFF
+
+set H3_APP_GITHUB="https://github.com/Microchip-MPLAB-Harmony"
+set APP_BRIDGE_GITHUB="https://github.com/zabooh"
+
+set H3_INSTALL_APP="..\frameworks\app"
+set H3_INSTALL_BOOTLOADER="..\frameworks\bootloader"
+
+set H3_INSTALL_APP_APPS="..\apps\apps"
+set H3_INSTALL_BOOTLOADER_APPS="..\apps\bootloader_apps"
+
+
+echo ========== Version Tags of H3 Framework for App ============
+echo echo|set /p="core                                "
+git -C %H3_INSTALL_APP%/core describe
+echo echo|set /p="csp                                 "
+git -C %H3_INSTALL_APP%/csp describe
+echo echo|set /p="wireless_wifi                       " 
+git -C %H3_INSTALL_APP%/wireless_wifi describe
+echo echo|set /p="usb                                 "
+git -C %H3_INSTALL_APP%/usb describe
+echo echo|set /p="dev_packs                           "
+git -C %H3_INSTALL_APP%/dev_packs describe
+echo echo|set /p="wireless_system_pic32mzw1_wfi32e01  "
+git -C %H3_INSTALL_APP%/wireless_system_pic32mzw1_wfi32e01 describe
+echo echo|set /p="wolfssl                             "
+git -C %H3_INSTALL_APP%/wolfssl describe
+echo echo|set /p="net                                 "
+git -C %H3_INSTALL_APP%/net describe
+echo echo|set /p="crypto                              "
+git -C %H3_INSTALL_APP%/crypto describe
+echo echo|set /p="CMSIS-FreeRTOS                      "
+git -C %H3_INSTALL_APP%/CMSIS-FreeRTOS describe
+echo echo|set /p="mhc                                 "
+git -C %H3_INSTALL_APP%/mhc describe
+
+echo ======== Version Tags of H3 Framework for Bootlader ========
+echo echo|set /p="csp                                 "
+git -C %H3_INSTALL_BOOTLOADER%/csp describe
+echo echo|set /p="core                                " 
+git -C %H3_INSTALL_BOOTLOADER%/core describe
+echo echo|set /p="usb                                 "
+git -C %H3_INSTALL_BOOTLOADER%/usb describe
+echo echo|set /p="dev_packs                           "
+git -C %H3_INSTALL_BOOTLOADER%/dev_packs describe
+echo echo|set /p="bootloader                          "
+git -C %H3_INSTALL_BOOTLOADER%/bootloader describe
+echo echo|set /p="bsp                                 "
+git -C %H3_INSTALL_BOOTLOADER%/bsp describe
+echo echo|set /p="mhc                                 "
+git -C %H3_INSTALL_APP%/mhc describe
+
+echo ============= Version Tags of wfi32 bridge =================
+echo echo|set /p="wfi32_bridge                        "
+git -C %H3_INSTALL_APP_APPS%/wfi32_bridge describe
+
+echo =========== Version Tags of Bootloader Apps ================
+echo echo|set /p="bootloader_apps_usb                 "
+git -C %H3_INSTALL_BOOTLOADER_APPS%/bootloader_apps_usb describe
+
+
+
+
